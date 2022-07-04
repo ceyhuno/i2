caa48ca08adc79c40d51ebeb3bfea7db6d60017f
ceyhuno <50355403+ceyhuno@users.noreply.github.com>
2022-07-04 22:10:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,21 @@ constants = ikea_api.Constants(country="de", language="de")
 
 token = ikea_api.Auth(constants).get_guest_token()
 
-services = await ikea_api.get_delivery_services(
-    constants=constants,
-    token=token,
-    items={
-        "30457903": 1,
-        "11111111": 2,  # invalid item that will be skipped
-    },
-    zip_code="101000",
-)
-print(f"services.delivery_options {services.delivery_options}")
-print(f"services.cannot_add {services.cannot_add}")
+import asyncio
+
+async def function(param) -> asyncio.coroutine:
+    services = await ikea_api.get_delivery_services(
+        constants=constants,
+        token=token,
+        items={
+            "30457903": 1,
+            "11111111": 2,  # invalid item that will be skipped
+        },
+        zip_code="101000",
+    )
+    print(f"services.delivery_options {services.delivery_options}")
+    print(f"services.cannot_add {services.cannot_add}")
+
+
+asyncio.run(function(whatever))
+
